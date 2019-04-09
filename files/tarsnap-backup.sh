245564349ae7cf47dd674a5e41849ade523d6ba9
Ciprian Manea <ciprian.manea@gmail.com>
2019-04-09 19:06:14 +0300

    improving quoting yet again

--- files/tarsnap-backup.sh
+++ files/tarsnap-backup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir -p "{{ backup_root }}"/backup/cache
-mkdir -p "{{ backup_root }}"/backup/"{{ appshome }}"
+mkdir -p "{{ backup_root }}/backup/cache"
+mkdir -p "{{ backup_root }}/backup/{{ appshome }}"
 # lets secure a bit this backup/dir structure
-chmod 700 "{{ backup_root }}"/backup
+chmod 700 "{{ backup_root }}/backup"
 
 DBALL=$(find /var/lib/mysql -maxdepth 1 -type d | xargs -n 1 basename | \
   grep -vE '^(sys|mysql|performance_schema)$')
@@ -40,13 +40,13 @@ xtrabackup --prepare --export --target-dir="$FULL" &>> "$LOG" && echo OK
 echo
 echo -n "dumping all rails apps uploads to backup directory "
 tar cf - "{{ appshome }}"/*/shared/public/uploads/ | \
-  tar xvf - -C "{{ backup_root }}"/backup/ &>> "$LOG" && echo OK
+  tar xvf - -C "{{ backup_root }}/backup/" &>> "$LOG" && echo OK
 echo
 echo -n "dumping all SSL certificates to backup directory "
 tar cf - "{{ certroot }}" | \
-  tar xvf - -C "{{ backup_root }}"/backup/ &>> "$LOG" && echo OK
+  tar xvf - -C "{{ backup_root }}/backup/" &>> "$LOG" && echo OK
 echo
 echo running tarsnap/backup now
-/usr/bin/tarsnap --print-stats --cachedir "{{ backup_root }}"/backup/cache \
+/usr/bin/tarsnap --print-stats --cachedir "{{ backup_root }}/backup/cache" \
   -c -f "$(uname -n)-$(date +%Y-%m-%d_%H-%M-%S)" \
-  "{{ backup_root }}"/backup | tee -a "$LOG"
+  "{{ backup_root }}/backup" | tee -a "$LOG"
